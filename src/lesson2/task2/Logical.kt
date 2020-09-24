65d3865868470df30a72b0d9f2bb5c90245cd711
AsDeep <trplkv@gmail.com>
2020-09-24 16:39:00 +0300

    lesson2_1_2

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -18,8 +18,8 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean =
-    number / 1000 + number / 100 - (number / 1000) * 10 == number % 100 / 10 + number % 10
+fun isNumberHappy(number: Int): Boolean = TODO()
+
 /**
  * Простая (2 балла)
  *
@@ -27,8 +27,8 @@ fun isNumberHappy(number: Int): Boolean =
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
-    (x1 == x2) || (y1 == y2) || (sqr(y1 - y2) == sqr(x1 - x2))
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+
 
 /**
  * Простая (2 балла)
@@ -36,17 +36,8 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int {
-    when (month) {
-        1, 3, 5, 7, 8, 10, 12 -> return 31
-        4, 6, 9, 11 -> return 30
-    }
-    if (month == 2) {
-        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) return 29
-        else return 28
-    }
-    return -1
-}
+fun daysInMonth(month: Int, year: Int): Int = TODO()
+
 /**
  * Простая (2 балла)
  *
