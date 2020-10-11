23f4cb4c365c4cab56546136047580e5d41a2e77
AsDeep <trplkv@gmail.com>
2020-10-11 18:06:51 +0300

    lesson4_final

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -4,7 +4,7 @@ package lesson4.task1
 
 import lesson1.task1.discriminant
 import kotlin.math.sqrt
-import kotlin.math.sqr
+import lesson1.task1.sqr
 import kotlin.math.pow
 import lesson3.task1.isPrime
 
